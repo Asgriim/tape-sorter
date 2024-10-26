0f879458908841fb22394254bd4b1eac2ac043af
Asgriim <andrew.vlasenkov70@gmail.com>
2024-10-26 19:59:10 +0300

    fix conf mistake

--- tapes/src/config/config.cpp
+++ tapes/src/config/config.cpp
@@ -7,7 +7,7 @@ tape::Config tape::read_conf(std::string_view path)  {
     std::ifstream ifstream(path.data());
     Config config;
     if (ifstream.is_open()) {
-        std::vector<std::string> names = {"read_delay", "write_delay", "shiftDelay", "in_memory_limit"};
+        std::vector<std::string> names = {"read_delay", "write_delay", "shift_delay", "in_memory_limit"};
         CFG::ReadFile(ifstream, names, config.readDelay, config.writeDelay, config.shiftDelay, config.inMemoryLimit);
     }
     return config;
